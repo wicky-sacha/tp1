dca978a51c02fabb738c6166f4aa535ab13d7999
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-09-09 13:42:47 +0200

    importation components

---
+++ src/components/card.vue
@@ -0,0 +1,42 @@
+<script>
+import bath from "./icones/bath.vue";
+import heart from "./icones/heart.vue";
+
+export default {
+  /* Les "props" servent à définir les "attributs" qui seront passés a l'instance du composant pour le personnalisé.
+  Chaque "props" a un nom et un type */
+  props: {
+    nom: String,
+    prix: Number,
+    favori: Boolean,
+    image: String, // les images sont simplement l'URL absolue (depuis la racine, débute par '/' )
+    nbrSDB: Number,
+  },
+  components: { bath, heart },
+};
+</script>
+
+<template>
+  <figure>
+    <!-- Pour changer la valeur d'un attribut on utilise un "binding" :
+    https://vuejs.org/guide/essentials/template-syntax.html#attribute-bindings 
+    Ici on affecte à l'attribut 'src' la 'props' "image" -->
+    <img class="w-full h-48 object-cover" :src="image" alt="" />
+    <figcaption
+      class="relative flex flex-col border-2 border-t-0 border-indigo-100"
+    >
+      <!-- Pour changer une classe CSS en fonction d'un booléen : 
+      https://vuejs.org/guide/essentials/class-and-style.html#binding-html-classes -->
+      <heart :class="{ 'fill-red-300': favori }" />
+
+      <!-- Pour afficher du contenu textuel, simplement utiliser l'interpolation par double accolades (qui contiendront le JS dont l'interpretation donne le résultat affiché). -->
+      <div class="text-2xl font-bold text-indigo-500 pr-1">${{ prix }}</div>
+
+      <h3 class="text-2xl text-gray-900">{{ nom }}</h3>
+
+      <hr class="border-indigo-100 border-t-2 my-4" />
+
+      <div><bath class="inline-block pr-1" />{{ nbrSDB }} Bathrooms</div>
+    </figcaption>
+  </figure>
+</template>
---
+++ src/components/icones/bath.vue
@@ -0,0 +1,40 @@
+<template>
+  <svg
+    width="20"
+    height="20"
+    viewBox="0 0 20 20"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    class="flex-grow-0 flex-shrink-0 w-5 h-5 relative"
+    preserveAspectRatio="xMidYMid meet"
+  >
+    <path
+      d="M3.33333 10H16.6667C16.8877 10 17.0996 10.0878 17.2559 10.2441C17.4122 10.4004 17.5 10.6123 17.5 10.8333V13.3333C17.5 14.2174 17.1488 15.0652 16.5237 15.6904C15.8986 16.3155 15.0507 16.6667 14.1667 16.6667H5.83333C4.94928 16.6667 4.10143 16.3155 3.47631 15.6904C2.85119 15.0652 2.5 14.2174 2.5 13.3333V10.8333C2.5 10.6123 2.5878 10.4004 2.74408 10.2441C2.90036 10.0878 3.11232 10 3.33333 10V10Z"
+      stroke="#6366F1"
+      stroke-width="1.66667"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+    <path
+      d="M5 10V4.16667C5 3.72464 5.17559 3.30072 5.48816 2.98816C5.80072 2.67559 6.22464 2.5 6.66667 2.5H9.16667V4.375"
+      stroke="#6366F1"
+      stroke-width="1.66667"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+    <path
+      d="M3.33337 17.5L4.16671 16.25"
+      stroke="#6366F1"
+      stroke-width="1.66667"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+    <path
+      d="M16.6667 17.5L15.8334 16.25"
+      stroke="#6366F1"
+      stroke-width="1.66667"
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    ></path>
+  </svg>
+</template>
