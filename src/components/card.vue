7a2156c5f889c6ff12dcbea5e0b59ca90531236c
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-09-09 14:40:44 +0200

    card fix

--- src/components/card.vue
+++ src/components/card.vue
@@ -1,38 +1,75 @@
-<script setup lang"ts">
-import bath from "./icones/bath.vue";
-import heart from "./icones/heart.vue";
-
-
-defineProps({
-    nom: String,
-    prix: Number,
-    favori: Boolean,
-    image: String, // les images sont simplement l'URL absolue (depuis la racine, débute par '/' )
-    nbrSDB: Number,
-    });
-</script>
-
 <template>
-  <figure>
-    <!-- Pour changer la valeur d'un attribut on utilise un "binding" :
-    https://vuejs.org/guide/essentials/template-syntax.html#attribute-bindings 
-    Ici on affecte à l'attribut 'src' la 'props' "image" -->
-    <img class="w-full h-48 object-cover" :src="image" alt="" />
+  <figure class="m-4">
+    <img class="w-full rounded-t-lg" v-bind:src="img" alt="" />
     <figcaption
-      class="relative flex flex-col border-2 border-t-0 border-indigo-100"
+      class="relative flex flex-col rounded-b-lg border-2 border-t-0 px-5 py-7"
     >
-      <!-- Pour changer une classe CSS en fonction d'un booléen : 
-      https://vuejs.org/guide/essentials/class-and-style.html#binding-html-classes -->
-      <heart :class="{ 'fill-red-300': favori }" />
-
-      <!-- Pour afficher du contenu textuel, simplement utiliser l'interpolation par double accolades (qui contiendront le JS dont l'interpretation donne le résultat affiché). -->
-      <div class="text-2xl font-bold text-indigo-500 pr-1">${{ prix }}</div>
-
       <h3 class="text-2xl text-gray-900">{{ nom }}</h3>
-
-      <hr class="border-indigo-100 border-t-2 my-4" />
-
-      <div><bath class="inline-block pr-1" />{{ nbrSDB }} Bathrooms</div>
+      <address class="not-italic text-gray-500">{{ txt }}</address>
+      <div class="order-first flex items-center">
+        <div class="text-2xl text-indigo-500">
+          ${{ price.toLocaleString("en-US") }}
+        </div>
+        <div class="text-gray-500">/mois</div>
+      </div>
+      <hr class="my-2 border-t-2 border-indigo-100" />
+      <div class="flex justify-between text-sm">
+        <div><bed class="inline-block align-top" /> 4 Beds</div>
+        <div><bath class="inline-block align-top" /> {{ nbbath }}</div>
+        <div><size class="inline-block align-top" /> {{ nbsize }}</div>
+      </div>
+      <div
+        class="
+          absolute
+          top-7
+          right-4
+          grid
+          h-12
+          w-12
+          place-items-center
+          rounded-full
+          border-2 border-indigo-100
+        "
+      >
+        <heart :class="{ 'fill-red-400': favoris }" />
+      </div>
     </figcaption>
   </figure>
 </template>
+
+<script setup lang="ts">
+
+import bed from "./icones/bed.vue";
+import bath from "./icones/bath.vue";
+import size from "./icones/size.vue";
+import heart from "./icones/hearth.vue";
+defineProps ({
+    price: {
+      type: Number,
+      default: 2700,
+    },
+    favoris: {
+      type: Boolean,
+      default: false,
+    },
+    nom: {
+      type: String,
+      default: "Beverly Springfield",
+    },
+    txt: {
+      type: String,
+      default: "2821 Lake Sevilla, Palm Harbor, TX",
+    },
+    img: {
+      type: String,
+    },
+    nbbath: {
+      type: String,
+      default: "2 Bathrooms",
+    },
+    nbsize: {
+      type: String,
+      default: "6x7.5 m²",
+    },
+});
+</script>
--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        
     </div>
     <card>
 </template>
