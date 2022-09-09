f09e2c7047f0c74145df5011b8d4a50fb66ed2bc
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-09-09 15:03:52 +0200

    liste fetch

---
+++ src/pages/liste-fetch.vue
@@ -0,0 +1,16 @@
+<template>
+    <div class="p-2">
+        <h1 class="text-2xl">Page Liste</h1>
+        <div v-for="maisons in maisons" :key="maisons.nom">
+            <card class="w-1/2" v-bind="maisons"/>
+        </div>
+    </div>
+    </template>
+
+<script setup lang="ts">
+    import card from "../components/card.vue";
+
+    const res = await fetch("maisons.json");
+    const maisons = await res.json();
+
+</script>
