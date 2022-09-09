d5cd806c0a26523f71d943f164d9ed37e9386eaf
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-09-09 14:45:27 +0200

    img card and list

--- src/pages/liste.vue
+++ src/pages/liste.vue
@@ -1,3 +1,15 @@
-<script>
-const maisons = [ {/* Obj 1re maison */}, { /* Obj 2nd */ }, /* ... */ ];
+<template>
+    <div class="p-2">
+        <h1 class="text-2xl">Page Liste</h1>
+        <div v-for="maisons in maisons" :key="maisons.nom">
+            <card class="w-1/2" v-bind="maisons"/>
+        </div>
+    </div>
+    </template>
+
+<script setup lang="ts">
+    import card from "../components/card.vue";
+
+  const maisons = [{nom:"test", price:34, favoris:true, txt:"produit de test", nbbath:2, nbsize:"test m²", img:"/public/card.jpg"}, {nom:"test2", price:27, favoris:false, txt:"2ème produit de test", nbbath:2, nbsize:"test*2 m²", img:"/public/card.jpg"}, {nom:"Dernier test", price:86, txt:"Dernier produit de test", nbbath:1, nbsize:"test m²", img:"/public/card.jpg"}]
+//   import maisons from "../assets/maisons.json";
 </script>
--- src/pages/simple.vue
+++ src/pages/simple.vue
@@ -1,13 +1,12 @@
 <template>
-    <div>
+    <div class="p-2">
+        <h1 class="text-2xl">Page Simple</h1>
+        <card class="w-1/2" v-bind="objet"/>
     </div>
-    <card>
-</template>
-
-<script setup lang='ts'>
-import Card from '../components/card.vue'
-const props =defineProps({
-    foo : String
-})
-</script>
+    </template>
     
+    <script setup lang="ts">
+        import card from "../components/card.vue";
+    
+        const objet = {nom:"test", price:34, favoris:true, txt:"produit de test", nbbath:2, nbsize:"test m²", img:"/public/card.jpg"}
+    </script>
