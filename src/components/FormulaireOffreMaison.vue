a720d7062eb808ef3087a89472507e3839e3d914
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-09-16 15:20:19 +0200

    verif maison

--- src/components/FormulaireOffreMaison.vue
+++ src/components/FormulaireOffreMaison.vue
@@ -12,14 +12,13 @@
         </div>
         <div class="p-2">
             <FormKit type="form" v-model="maison" :config="{
- classes: {
- input: 'p-1 rounded border-gray-300 shadow-sm border',
- label: 'text-gray-600',
- },
- }">
+                classes: {
+                    input: 'p-1 rounded border-gray-300 shadow-sm border',
+                    label: 'text-gray-600',},
+                    }">
                 <FormKit name="nom" label="nom" />
                 <FormKit name="prix" label="prix" type="number" />
-                <FormKit name="favori" label="mettre en valeur" type="checkbox" />
+                <FormKit    name="favori"   label="mettre en valeur" type="checkbox"    wrapper-class="flex"/>
             </FormKit>
         </div>
     </div>
