b933ebc83a0456c58b5fd2cb2458b177c7f0aa69
wicky-sacha <sacha.wicky@edu.univ-fcomte.fr>
2022-09-16 14:51:59 +0200

    formkit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-content: [
+  content: [
     "./index.html",
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    formKitTailwind
+  ],
 }
+
